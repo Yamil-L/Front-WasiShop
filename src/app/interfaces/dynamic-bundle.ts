8aa60e13f6e75140afca66f2a953791ea153d26c
rcayroc02 <roberto.cayro@ucsp.edu.pe>
2025-09-24 11:45:30 -0500

    Revert "add-client fixed"
    This reverts commit 30bbfad3de7fe1670218c4f184f1c05b460aef24.

--- src/app/interfaces/dynamic-bundle.ts
+++
@@ -1,9 +0,0 @@
-// src/app/interfaces/dynamic-bundle.ts
-export interface DynamicBundle {
-  id: string;
-  name: string;
-  description: string;
-  discount_percent: number;
-  created_date: string;
-  imageUrl: string; // exclusivo para los dinámicos
-}
